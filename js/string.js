4fc57febadacfb64719fe30909f786327f60ec0a
Owen Rowe <row20015@byui.edu>
2022-01-28 17:28:48 -0700

    fdone

---
+++ js/string.js
@@ -0,0 +1,17 @@
+
+        document.querySelector('#copyright').textContent = new Date().getFullYear();
+
+        function storyComplete() {
+	// INPUT
+	    firstname = document.querySelector('#firstname').value;
+        fruit = document.querySelector('#fruit').value;
+        animal1 = document.querySelector('#animal1').value;
+        animal2 = document.querySelector('#animal2').value;
+
+	// PROCESSING
+	 
+	
+	//OUTPUT - Example output: "And it came to pass that the people of ___ did till the land, and raise all manner of ___, and flocks of herds, and flocks of all manner of ___ of every kind, and ___."
+
+	 document.querySelector("#output").innerHTML = `And it came to pass that the people of ${firstname} did till the land, and raise all manner of ${fruit}, and flocks of herds, and flocks of all manner of ${animal1} of every kind, and ${animal2}.`
+        }
